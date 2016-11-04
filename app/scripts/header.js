1c459dacf491108c86ee08e3437229ddc849dab8
Caryn Ligon <carynligon@gmail.com>
2016-11-04 09:17:09 -0500

    added nav to header

--- app/scripts/header.js
+++ app/scripts/header.js
@@ -1,5 +1,18 @@
-const header = (`<header><img src="/assets/images/logo-zebra.png"/>
+const header = (`
+  <header>
+    <img src="/assets/images/logo-zebra.png"/>
     <h1>The New York City Guide to Getting Around.</h1>
-    <p>We know that New York is exciting but it can also be intimidating. The secrets to navigating New York doesn’t just have to be for locals. The Zebra has put together this simple guide to getting around so you can navigate New York like a pro.</p></header>`)
+    <p>We know that New York is exciting but it can also be intimidating. The secrets to navigating New York doesn’t just have to be for locals. The Zebra has put together this simple guide to getting around so you can navigate New York like a pro.</p>
+  </header>
+  <nav>
+    <ul class="nav-list">
+      <a href="#"><li>Compare Car Insurance</a>
+      <li>></li>
+      <a href="#"><li>New York</a>
+      <li>></li>
+      <a href="#"><li>New York City Transportation Guide</li></a>
+    </ul>
+  </nav>
+    `)
 
 export default header;
