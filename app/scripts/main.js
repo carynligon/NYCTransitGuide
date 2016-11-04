31f43289bee5187099b66276bb87f4cbd0940984
Caryn Ligon <carynligon@gmail.com>
2016-11-04 11:19:50 -0500

    structure almost done

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import header from './header';
 import categories from './categories';
+import airportSection from './airportSection';
 
 const container = document.getElementById('container');
 //
@@ -13,6 +14,7 @@ document.addEventListener('DOMContentLoaded', renderContent);
 function renderContent() {
   $('#container').append(header);
   $('#container').append(categories);
+  $('#container').append(airportSection);
   // container.innerHTML = header;
   // document.getElementById('container').appendChild(categories);
 }
